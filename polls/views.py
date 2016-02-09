24ff61e7d5c6611e649ae6c1b22a8649c6116d39
jlautarosilva <jlautarosilva@gmail.com>
2016-02-09 18:11:18 -0300

    Tutorial 03 con shortcut render

--- polls/views.py
+++ polls/views.py
@@ -3,17 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-from django.template import loader
-
+#from django.template import loader
+from django.shortcuts import render
 from .models import Question
+
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    template = loader.get_template('polls/index.html')
+#    context = {
+#        'latest_question_list': latest_question_list,
+#    }
+#    return HttpResponse(template.render(context, request))
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
-        'latest_question_list': latest_question_list,
+        'latest_question_list':latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request,'polls/index.html',context)
+
 
 def detail(request, question_id):
     return HttpResponse("esta es la pregunta numero %s." % question_id)
