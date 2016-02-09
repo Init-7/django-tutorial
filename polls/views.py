9ba913e6e945b573ed9b9b729915cb4933a31684
jlautarosilva <jlautarosilva@gmail.com>
2016-02-09 18:06:01 -0300

    Tutorial 03 vistas y templates

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hola mundo, estas en el index de polls")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("esta es la pregunta numero %s." % question_id)
+
+def results(request, question_id):
+    response = "Este es el resultado de la pregunta %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Estas votando en la pregunta %s." % question_id)
