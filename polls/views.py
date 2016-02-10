9b8290761b458f94f15bf6968830742f1fbf916a
jlautarosilva <you@example.com>
2016-02-10 01:25:41 -0300

    Tutorial 03 con shortcut y template de error 404

--- polls/views.py
+++ polls/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-#from django.template import loader
-from django.shortcuts import render
+
+from django.shortcuts import get_object_or_404, render
 from .models import Question
+
+#from django.http import Http404
 
 #def index(request):
 #    latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -23,8 +25,16 @@ def index(request):
     return render(request,'polls/index.html',context)
 
 
+#def detail(request, question_id):
+#    try:
+#        question = Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+#        raise Http404("Question does not exist")
+#    return render(request,'polls/detail.html',{'question':question})
+
 def detail(request, question_id):
-    return HttpResponse("esta es la pregunta numero %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "Este es el resultado de la pregunta %s"
